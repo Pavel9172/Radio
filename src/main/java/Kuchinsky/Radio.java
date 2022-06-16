efd65257732b02653e1cd620dd48268cc1e97cb0
Pavel9172 <pavlo_k@mail.ru>
2022-06-16 16:48:17 +0500

    error increaseRadioVolume

--- src/main/java/Kuchinsky/Radio.java
+++ src/main/java/Kuchinsky/Radio.java
@@ -1,5 +1,28 @@
 package Kuchinsky;
 
 public class Radio {
-    
+
+   public int RadioVolume;
+   public int getRadioVolume(){
+      return RadioVolume;
+   }
+
+   public void setRadioVolume(int RadioVolume){
+      if (RadioVolume < 0){
+         return;
+      }
+      if (RadioVolume > 10){
+         return;
+      }
+      this.RadioVolume = RadioVolume;
+   }
+   public void increaseRadioVolume(int RadioVolume) {
+      if (RadioVolume < 10) {
+         RadioVolume = RadioVolume + 1;
+         return;
+
+      }
+   }
+
+
 }
--- src/test/java/Kuchinsky/RadioTest.java
+++ src/test/java/Kuchinsky/RadioTest.java
@@ -1,7 +1,21 @@
 package Kuchinsky;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+
+    @Test
+    public void test(){
+        Radio cound = new Radio();
+        //cound.RadioVolume = 8;
+       cound.setRadioVolume(6);
+      cound.increaseRadioVolume(6);
+        int expected = 7;
+        int actual = cound.getRadioVolume();
+        assertEquals(expected, actual);
+
+    }
 
 }
